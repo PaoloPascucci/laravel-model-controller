2134b91b5102903b11ed5e01dd943f27d0e1ef13
Paolo <pascuccip9@gmail.com>
2022-01-11 14:37:28 +0100

    Add starting layout

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <h1>Movies</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, inventore.</p>
+
+    <section class="books">
+        <p>Check out books selection</p>
+    </section>
+</div>
+
+@endsection
